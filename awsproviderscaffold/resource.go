b7c1f993351ea6cb1f61922342e71dc66ddf67c0
Simon Davis <sdavis@hashicorp.com>
2021-12-24 13:51:46 -0800

    found target insert point

--- awsproviderscaffold/resource.go
+++ awsproviderscaffold/resource.go
@@ -3,9 +3,13 @@ package main
 import (
 	"flag"
 	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
 	"log"
 	"os"
 	"path/filepath"
+	"sort"
 	"strings"
 	"text/template"
 )
@@ -25,6 +29,8 @@ type ResourceCommand struct {
 func (c *ResourceCommand) Run(args []string) int {
 	//TODO Flag to include tag boilerplate
 	//TODO Validate pkg name exists.
+
+	c.Parse()
 
 	c.Flags().Parse(args)
 
@@ -85,7 +91,11 @@ func execTemplate(tmpl string, tmplType TemplateType, config map[string]string) 
 		return err
 	}
 
-	f, err := os.Create(filepath.Join(pwd, "..", "internal", "service", config["pkg"], config["name"]+suffix+".go"))
+	pkgDir := filepath.Join(pwd, "..", "internal", "service", config["pkg"])
+
+	_ = os.MkdirAll(pkgDir, os.ModePerm)
+
+	f, err := os.Create(filepath.Join(pkgDir, config["name"]+suffix+".go"))
 	if err != nil {
 		log.Println("create file: ", err)
 		return err
@@ -108,7 +118,146 @@ func (c *ResourceCommand) Synopsis() string {
 func (c *ResourceCommand) Help() string {
 	helpText := `
 Usage: awsproviderscaffold resource [options] 
-Creates a templated resource belonging to an AWS service package.` + c.Flags().Usage
 
-	return strings.TrimSpace(helpText)
+Creates a templated resource belonging to an AWS service package.
+`
+
+	f := c.Flags()
+
+	f.VisitAll(func(f *flag.Flag) {
+		helpText += fmt.Sprintf("\n %s: %s", f.Name, f.Usage)
+	})
+
+	return helpText
+}
+
+func (c *ResourceCommand) Parse() string {
+
+	pkg := "s3"
+	resource := "aws_s3_zoo"
+
+	fset := token.NewFileSet()
+	file, err := parser.ParseFile(fset, "../internal/provider/provider.go", nil, parser.ParseComments)
+
+	if err != nil {
+		log.Println("Unable to parse provider.go")
+	}
+	var stack []ast.Node
+
+	ast.Inspect(file, func(n ast.Node) bool {
+		// s := ""
+		// switch x := n.(type) {
+		// case *ast.Ident:
+		// 	s = x.Name
+
+		// 	// Called recursively.
+		// 	if s == "DataSourcesMap" {
+		// 		ast.Print(fset, n)
+		// 		return false
+		// 	}
+		// }
+		// return true
+
+		if n, ok := n.(*ast.Ident); ok {
+			if n.Name == "DataSourcesMap" {
+
+				dsMap := stack[len(stack)-1]
+
+				var thing ast.Expr
+				switch x := dsMap.(type) {
+				case *ast.KeyValueExpr:
+					thing = x.Value
+				}
+
+				var thng ast.CompositeLit
+
+				switch x := thing.(type) {
+				case *ast.CompositeLit:
+					thng = *x
+				}
+
+				var pkgRes = make(map[string]int)
+
+			out:
+				for i := 0; i < len(thng.Elts); i++ {
+					var top ast.KeyValueExpr
+					switch x := thng.Elts[i].(type) {
+					case *ast.KeyValueExpr:
+						top = *x
+					}
+
+					var lit ast.BasicLit
+					switch x := top.Key.(type) {
+					case *ast.BasicLit:
+						lit = *x
+					}
+
+					var call ast.CallExpr
+					switch x := top.Value.(type) {
+					case *ast.CallExpr:
+						call = *x
+					}
+
+					var sel ast.SelectorExpr
+					switch x := call.Fun.(type) {
+					case *ast.SelectorExpr:
+						sel = *x
+					}
+
+					var ide ast.Ident
+					switch x := sel.X.(type) {
+					case *ast.Ident:
+						ide = *x
+					}
+
+					if ide.Name == pkg {
+						pkgRes[lit.Value[1:len(lit.Value)-1]] = i
+					}
+
+					if ide.Name != pkg && len(pkgRes) > 0 {
+						keys := make([]string, 0, len(pkgRes))
+						for k := range pkgRes {
+							keys = append(keys, k)
+						}
+
+						sort.Strings(keys)
+
+						j := 0
+						for _, key := range keys {
+							//log.Println(key)
+							j++
+							if key > resource || j == len(pkgRes) {
+								log.Printf("%d %d", j, len(pkgRes))
+								log.Printf("%s %s %d", key, resource, pkgRes[key])
+								break out
+							}
+						}
+						//break out
+
+					}
+
+				}
+
+				//log.Println(thng.Elts)
+
+			}
+		}
+
+		// Manage the stack. Inspect calls a function like this:
+		//   f(node)
+		//   for each child {
+		//      f(child) // and recursively for child's children
+		//   }
+		//   f(nil)
+		if n == nil {
+			// Done with node's children. Pop.
+			stack = stack[:len(stack)-1]
+		} else {
+			// Push the current node for children.
+			stack = append(stack, n)
+		}
+
+		return true
+	})
+	return ""
 }

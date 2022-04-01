f01be15a3bf4b418c540de6ccd74619275c977d9
Graham Davison <gdavison@hashicorp.com>
2022-04-01 16:54:44 -0700

    Allow envvar `GO_VERSION` to override local goenv configuration

--- .teamcity/scripts/setup_goenv.sh
+++ .teamcity/scripts/setup_goenv.sh
@@ -7,4 +7,13 @@ printf '\nUpdating goenv to %s...\n' "${GOENV_TOOL_VERSION}"
 git pull origin "${GOENV_TOOL_VERSION}"
 popd
 
-goenv install -s "$(goenv local)" && goenv rehash
+go_version=$(goenv local 2>/dev/null)
+echo "Local Go version: ${go_version}"
+
+if [ -z "$GO_VERSION" ]; then
+  echo "\$GO_VERSION: ${GO_VERSION}"
+  go_version=$GO_VERSION
+fi
+
+echo "Installing Go version $go_version..."
+goenv install --skip-existing "$go_version" && goenv rehash
